596c6c05fef8a4234c7f301dbea664294e94cbe7
HeidiW <foywilliams@gmail.com>
2014-10-05 14:20:13 -0400

    Trying to add yesterday's commits again

--- nurse_jackie_contacts/public/javascripts/scripts.js
+++ nurse_jackie_contacts/public/javascripts/scripts.js
@@ -2,7 +2,7 @@ console.log("hello")
 
 var nurses = ["Jackie Peyton", "Zoe Barkow", "Thor Lundgren"];
 var doctors = ["Fitch Cooper", "Elenor O'Hara", "Carrie Roman"];
-var administators = ["Gloria Akalitus"];
+var administrators = ["Gloria Akalitus"];
 var pharmacists = ["Eddie Walzer"]
 
 var addNursesButton = document.getElementById('addbutton');
@@ -11,70 +11,70 @@ addNursesButton.addEventListener("click", function(){
 	var nursediv = document.getElementById("nurses");
 	var ul = document.getElementById('nursesul');
      
-    // for (var i = 0; i < nurses.length; i++); 
-    // 	var addnurse = document.createElement("li");
-  	//  addnurse.innerText = nurses[i];
-    // 	ul.appendChild(addnurse);
-
     for (var i = 0; i < nurses.length; i++) {
     	var listItem = document.createElement("li");
-
     	var nurse = nurses[i]
-
     	listItem.innerText = nurse;
         ul.appendChild(listItem);
-    	
     }
-    
 });
 
 var addDoctorsButton = document.getElementById('adddoctor');
-
 addDoctorsButton.addEventListener("click", function(){
 	var doctordiv = document.getElementById("doctors");
 	var ul = document.getElementById('doctorsul');
-     
-    // for (var i = 0; i < nurses.length; i++); 
-    // 	var addnurse = document.createElement("li");
-  	//  addnurse.innerText = nurses[i];
-    // 	ul.appendChild(addnurse);
 
     for (var i = 0; i < doctors.length; i++) {
     	var listItem = document.createElement("li");
-
     	var doctor = doctors[i]
-
     	listItem.innerText = doctor;
         ul.appendChild(listItem);
-    	
     }
-    
 });
 
 var addAdministratorsButton = document.getElementById('addadministrators');
-
 addAdministratorsButton.addEventListener("click", function(){
 	var administratordiv = document.getElementById("administrators");
 	var ul = document.getElementById('administratorsul');
-     
-    // for (var i = 0; i < nurses.length; i++); 
-    // 	var addnurse = document.createElement("li");
-  	//  addnurse.innerText = nurses[i];
-    // 	ul.appendChild(addnurse);
 
     for (var i = 0; i < administrators.length; i++) {
     	var listItem = document.createElement("li");
-
     	var administrator = administrators[i]
-
     	listItem.innerText = administrator;
         ul.appendChild(listItem);
-    	
     }
-    
 });
 
+var addPharmacistsButton = document.getElementById('addpharmacists');
+addPharmacistsButton.addEventListener("click", function(){
+	var pharmacistdiv = document.getElementById("pharmacists");
+	var ul = document.getElementById('pharmacistsul');
 
+    for (var i = 0; i < pharmacists.length; i++) {
+    	var listItem = document.createElement("li");
+    	var pharmacist = pharmacists[i]
+    	listItem.innerText = pharmacist;
+        ul.appendChild(listItem);
+    }  
+});
+
+var newNursesButton = document.getElementById("createnurse");
+
+newNursesButton.addEventListener("click", function() {
+	var textbox = document.getElementById("newstaffnurse");
+	var text = textbox.value;
+	if(text != ""){
+                debugger
+
+		var listItem = document.createElement("ul");
+		listItem.innerHTML = "<li>" + text + "</li>";
+
+		var list = document.getElementById("list");
+		list.appendChild(listItem);
+
+		textbox.value = ""
+	}
+});
 
 // function addElToLanguages(text){
 // 	var list = document.getElementById("languages_list");
